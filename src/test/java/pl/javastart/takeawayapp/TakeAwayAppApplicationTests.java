ebeb5626a882175ee31996f337a74bae33313b11
Jakub Zaboronek <jakubzaboronek@wp.pl>
2019-07-26 11:01:45 +0200

    Initial commit

---
+++ src/test/java/pl/javastart/takeawayapp/TakeAwayAppApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.javastart.takeawayapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TakeAwayAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
